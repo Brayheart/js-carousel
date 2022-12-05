5eb9f7376f0348a07ac96609e10e1fa2af2f9d67
Tyler Bray <tylerhbray@gmail.com>
2022-12-04 18:49:18 -0800

    edit

--- index.js
+++ index.js
@@ -14,15 +14,3 @@ $('.item').each(function(){
       next.children(':first-child').clone().appendTo($(this));
     }
   });
-  
-  $(".carousel").swipe({
-  
-    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-  
-      if (direction == 'left') $(this).carousel('next');
-      if (direction == 'right') $(this).carousel('prev');
-  
-    },
-    allowPageScroll:"vertical"
-  
-  });
