533120b0da234c65e02e8673079be3cb578721df
Tyler Bray <tylerhbray@gmail.com>
2022-12-04 18:49:35 -0800

    edit

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 $('.item').each(function(){
-    var next = $(this).next();
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
     if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
+    	next = $(this).siblings(':first');
+  	}
     
-    for (var i=0;i<2;i++) {
-      next=next.next();
-      if (!next.length) {
-          next = $(this).siblings(':first');
-        }
-      
-      next.children(':first-child').clone().appendTo($(this));
-    }
-  });
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
+$(".carousel").swipe({
+
+  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+    if (direction == 'left') $(this).carousel('next');
+    if (direction == 'right') $(this).carousel('prev');
+
+  },
+  allowPageScroll:"vertical"
+
+});
